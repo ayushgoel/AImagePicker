10f26c5b8dbf9556dcb153080b52df3b4b109d00
Ayush Goel <ayush.g@directi.com>
2013-02-10 20:22:40 +0530

    Use the added button on toolbar to open a action sheet, to give user option to either use camera as source or his library

--- AImagePicker/AImagePicker/FirstScreenViewController.m
+++ AImagePicker/AImagePicker/FirstScreenViewController.m
@@ -10,7 +10,7 @@
 
 #define TOOLBAR_HEIGHT 30.0
 
-@interface FirstScreenViewController ()
+@interface FirstScreenViewController () <UIActionSheetDelegate>
 @property (nonatomic, strong) UIImageView *imageView;
 @property (nonatomic, strong) UIToolbar *toolBar;
 @end
@@ -25,8 +25,10 @@
   self.imageView = [[[UIImageView alloc] initWithFrame:self.view.bounds] autorelease];
   self.toolBar = [[[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height - TOOLBAR_HEIGHT, self.view.bounds.size.width, TOOLBAR_HEIGHT)] autorelease];
   self.toolBar.barStyle = UIBarStyleBlackTranslucent;
+
   UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(buttonPressed)];
   [self.toolBar setItems:@[button] animated:NO];
+
   [self.view addSubview:self.imageView];
   [self.view addSubview:self.toolBar];
 	// Do any additional setup after loading the view, typically from a nib.
@@ -39,7 +41,14 @@
 }
 
 - (void)buttonPressed {
-  NSLog(@"Button Pressed!");
+  UIActionSheet *pickSource = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Open Camera", @"Open Library", nil];
+  pickSource.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
+  [pickSource showInView:self.view];
+}
+
+#pragma UIActionSheetDelegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+
 }
 
 @end
