28b83342ca5f6666fbb2fd37f6fbfad4bfe637c0
Ayush Goel <ayush.g@directi.com>
2013-02-10 19:03:04 +0530

    Inititate project. Rename files.

---
+++ AImagePicker/AImagePicker/AIPAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AIPAppDelegate.h
+//  AImagePicker
+//
+//  Created by Ayush on 10/02/13.
+//  Copyright (c) 2013 Ayush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FirstScreenViewController;
+
+@interface AIPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FirstScreenViewController *viewController;
+
+@end
---
+++ AImagePicker/AImagePicker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AImagePicker
+//
+//  Created by Ayush on 10/02/13.
+//  Copyright (c) 2013 Ayush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AIPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AIPAppDelegate class]));
+  }
+}
