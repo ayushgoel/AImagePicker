a3f0a62940e39da8a5dcc2d50cdd60a9d308aba1
Ayush Goel <ayush.g@directi.com>
2013-02-10 23:04:44 +0530

    Add files to show image preview

---
+++ AImagePicker/AImagePicker/ImagePreViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagePreViewController.h
+//  AImagePicker
+//
+//  Created by Ayush on 10/02/13.
+//  Copyright (c) 2013 Ayush. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagePreViewController : UIViewController
+
+@end
---
+++ AImagePicker/AImagePicker/ImagePreViewController.m
@@ -0,0 +1,29 @@
+//
+//  ImagePreViewController.m
+//  AImagePicker
+//
+//  Created by Ayush on 10/02/13.
+//  Copyright (c) 2013 Ayush. All rights reserved.
+//
+
+#import "ImagePreViewController.h"
+
+@interface ImagePreViewController ()
+
+@end
+
+@implementation ImagePreViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
