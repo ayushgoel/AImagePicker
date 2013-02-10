f9ca38ace1ffbfba3aa0447663868604e4208325
Ayush Goel <ayush.g@directi.com>
2013-02-10 19:36:23 +0530

    Create the initial ViewController to be presented when app starts. The View has an ImageView to show the image selected, and a toolbar with a button. When the button is pressed, it should start the image choosing process.

--- AImagePicker/AImagePicker/FirstScreenViewController.m
+++ AImagePicker/AImagePicker/FirstScreenViewController.m
@@ -8,22 +8,38 @@
 
 #import "FirstScreenViewController.h"
 
-@interface FirstScreenViewController ()
+#define TOOLBAR_HEIGHT 30.0
 
+@interface FirstScreenViewController ()
+@property (nonatomic, strong) UIImageView *imageView;
+@property (nonatomic, strong) UIToolbar *toolBar;
 @end
 
 @implementation FirstScreenViewController
+@synthesize imageView = imageView_;
+@synthesize toolBar = toolBar_;
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
+  self.imageView = [[[UIImageView alloc] initWithFrame:self.view.bounds] autorelease];
+  self.toolBar = [[[UIToolbar alloc] initWithFrame:CGRectMake(0, self.view.bounds.size.height - TOOLBAR_HEIGHT, self.view.bounds.size.width, TOOLBAR_HEIGHT)] autorelease];
+  self.toolBar.barStyle = UIBarStyleBlackTranslucent;
+  UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(buttonPressed)];
+  [self.toolBar setItems:@[button] animated:NO];
+  [self.view addSubview:self.imageView];
+  [self.view addSubview:self.toolBar];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+- (void)buttonPressed {
+  NSLog(@"Button Pressed!");
 }
 
 @end
